8b62586c9abc78d54e31da7ac7df0c8bbe5d5d2c
Andrew Hall <andrew@rioja.io>
2020-06-16 21:08:03 +0100

    initial logic to load and query sqlite wasm db

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,41 @@
 import * as React from "react";
+import sqlData from "./data/sqlite-wordnet.sqlite";
+
+let worker = new Worker("../node_modules/sql.js/dist/worker.sql-wasm-debug.js");
+
+worker.addEventListener("message", ({ data }) => {
+  console.debug(`[WORKER] ${JSON.stringify(data)}`);
+});
+
+window
+  .fetch(sqlData)
+  .then((res) => res.arrayBuffer())
+  .then((sqlFile) => {
+    console.info(`Database size: ${sqlFile.byteLength}`);
+    const t1 = performance.now();
+    
+    worker.postMessage({
+      id: 1,
+      action: "open",
+      buffer: sqlFile,
+    });
+
+    const handleResponse = ({ data: { id } }) => {
+      if (id === 1) {
+        console.info(`Database loaded in ${performance.now() - t1}ms`);
+        worker.removeEventListener("message", handleResponse);
+        worker.postMessage({
+          id: 2,
+          action: "exec",
+          sql: `select * from words limit 10`,
+        });
+      }
+    };
+
+    worker.addEventListener("message", handleResponse);
+  });
+
+worker.onerror = (e) => console.log("Worker error: ", e);
 
 function App() {
   return <>Hello world</>;

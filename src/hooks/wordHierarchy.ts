f2e23bdda334e144856227fbaf902b6ff282bbcc
Andrew Hall <andrew@rioja.io>
2020-07-05 10:45:39 +0100

    optimise word hierarchy performance

--- src/hooks/wordHierarchy.ts
+++ src/hooks/wordHierarchy.ts
@@ -1,23 +1,20 @@
 import {
-  defaultTo,
-  filter,
-  head,
-  join,
+  equals,
+  identity,
+  ifElse,
+  is,
+  length,
   map,
-  mergeRight,
-  objOf,
   pipe,
-  prop,
+  filter,
   propEq,
-  propOr,
-  zipWith,
+  useWith,
 } from "ramda";
-import { useEffect, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { formatSql } from "../db";
-import { Routes } from "../routes";
-import { selectLocationType, selectWordId } from "../selectors/location";
-import { makeDbQuery } from "./db";
+import { DbResult, formatSql, getDbInstance } from "../db";
+import { mapResultToObjects } from "../helpers";
+import { selectWordId } from "../selectors/location";
 import { Word } from "./searchWord";
 
 export interface WordSenseResult {
@@ -28,64 +25,70 @@ export interface WordSenseResult {
   definition: string;
 }
 
-const useWordQuery = makeDbQuery<Word>(
-  (wordid: number) => formatSql`
+function formatWordQuery(wordid: number) {
+  return formatSql`
   SELECT wordid, lemma
   FROM words
-  WHERE wordid = ${wordid}
-  `
-);
+  WHERE wordid = ${wordid}`;
+}
 
-const useWordSensesQuery = makeDbQuery<WordSenseResult>(
-  (wordid: string) => formatSql`
+function formatWordSensesQuery(wordid: number) {
+  return formatSql`
   SELECT synsetid, senseid, sensenum, pos, definition
   FROM senses
   JOIN synsets USING (synsetid)
   WHERE wordid = ${wordid}
   ORDER BY pos, sensenum
-  `
-);
+  `;
+}
 
 export interface SenseLinkResult {
   synsetid: number;
+  synset2id: number;
   link: string;
   recurses: number;
   pos: string;
   definition: string;
 }
 
-const useSenseLinksQuery = makeDbQuery<SenseLinkResult>(
-  pipe(
-    map(
-      (synsetid: number) => formatSql`
-      SELECT 
-        synset2id AS synsetid,
-        link,
-        recurses,
-        synsets.pos,
-        definition
-      FROM semlinks
-      JOIN linktypes USING (linkid)
-      JOIN synsets ON synset2id = synsetid
-      JOIN lexdomains USING (lexdomainid)
-      WHERE synset1id = ${synsetid}
-      UNION
-      SELECT 
-        synset2id,
-        link,
-        recurses,
-        synsets.pos,
-        definition
-      FROM lexlinks
-      JOIN linktypes USING (linkid)
-      JOIN synsets ON synset2id = synsetid
-      JOIN lexdomains USING (lexdomainid)
-      WHERE synset1id = ${synsetid}
-      ORDER BY link, recurses;`
-    ),
-    join("\n")
-  ) as (s: number[]) => string
-);
+function formatSenseLinksQuery(wordid: number) {
+  return formatSql`
+  WITH word_senses AS (
+    SELECT synsetid
+    FROM senses
+    JOIN synsets USING (synsetid)
+    WHERE wordid = ${wordid}
+    ORDER BY pos, sensenum
+  )
+  SELECT 
+    word_senses.synsetid,
+    synset2id,
+    link,
+    recurses,
+    synsets.pos,
+    definition
+  FROM semlinks
+  JOIN linktypes USING (linkid)
+  JOIN synsets ON synset2id = synsets.synsetid
+  JOIN lexdomains USING (lexdomainid)
+  JOIN word_senses 
+    ON synset1id = word_senses.synsetid
+  UNION
+  SELECT 
+    word_senses.synsetid,
+    synset2id,
+    link,
+    recurses,
+    synsets.pos,
+    definition
+  FROM lexlinks
+  JOIN linktypes USING (linkid)
+  JOIN synsets ON synset2id = synsets.synsetid
+  JOIN lexdomains USING (lexdomainid)
+  JOIN word_senses 
+    ON synset1id = word_senses.synsetid
+  ORDER BY word_senses.synsetid, link, recurses`;
+}
 
 interface WordLinkResult {
   synsetid: number;
@@ -93,132 +96,120 @@ interface WordLinkResult {
   lemma: string;
 }
 
-const useWordLinksQuery = makeDbQuery<WordLinkResult>(
-  pipe(
-    (wordid: number) => formatSql`
-    SELECT synsetid, wordid, lemma
-    FROM (
-        SELECT s2.synsetid
-        FROM senses
-        JOIN synsets s1 USING (synsetid)
-        JOIN semlinks sl ON sl.synset1id = s1.synsetid
-        JOIN linktypes USING (linkid)
-        JOIN synsets s2 ON sl.synset2id = s2.synsetid
-        JOIN lexdomains USING (lexdomainid)
-        WHERE wordid = ${wordid}
-        UNION ALL
-        SELECT s2.synsetid
-        FROM senses
-        JOIN synsets s1 USING (synsetid)
-        JOIN lexlinks sl ON sl.synset1id = s1.synsetid
-        JOIN linktypes USING (linkid)
-        JOIN synsets s2 ON sl.synset2id = s2.synsetid
-        JOIN lexdomains USING (lexdomainid)
-        WHERE wordid = ${wordid}
-    ) t
-    JOIN senses USING (synsetid)
-    JOIN words USING (wordid)
-    GROUP BY synsetid, wordid, lemma
-    ORDER BY synsetid;`
-  ) as (s: number) => string
-);
-
-const selectLemma = pipe(
-  prop("results") as (r: any) => Word[],
-  head,
-  defaultTo([]) as (a: any) => [],
-  head,
-  defaultTo({}),
-  propOr("", "lemma")
-);
-
-const selectSenseSynsetIds = pipe(
-  prop("results") as (r: any) => WordSenseResult[],
-  head,
-  defaultTo([]) as (s: any) => [],
-  map(prop("synsetid"))
-);
-
-const selectSenses = pipe(
-  prop("results") as (r: any) => WordSenseResult[],
-  head
-) as (a: any) => WordSenseResult;
-
-const selectSenseLinks = pipe(
-  prop("results") as (r: any) => SenseLinkResult[][]
-);
-
-const selectWordLinks = pipe(
-  prop("results") as (r: any) => WordLinkResult[],
-  head
-) as (a: any) => WordLinkResult[];
+function formatWordLinksQuery(wordid: number) {
+  return formatSql`
+  SELECT synsetid, wordid, lemma
+  FROM (
+      SELECT s2.synsetid
+      FROM senses
+      JOIN synsets s1 USING (synsetid)
+      JOIN semlinks sl ON sl.synset1id = s1.synsetid
+      JOIN linktypes USING (linkid)
+      JOIN synsets s2 ON sl.synset2id = s2.synsetid
+      JOIN lexdomains USING (lexdomainid)
+      WHERE wordid = ${wordid}
+      UNION ALL
+      SELECT s2.synsetid
+      FROM senses
+      JOIN synsets s1 USING (synsetid)
+      JOIN lexlinks sl ON sl.synset1id = s1.synsetid
+      JOIN linktypes USING (linkid)
+      JOIN synsets s2 ON sl.synset2id = s2.synsetid
+      JOIN lexdomains USING (lexdomainid)
+      WHERE wordid = ${wordid}
+  ) t
+  JOIN senses USING (synsetid)
+  JOIN words USING (wordid)
+  GROUP BY synsetid, wordid, lemma
+  ORDER BY synsetid`;
+}
 
 export function useWordHierarchy() {
   const wordid = useSelector(selectWordId);
-  const [wordState, execWordQuery] = useWordQuery();
-  const [sensesState, execSensesQuery] = useWordSensesQuery();
-  const [senseLinksState, execSenseLinksQuery] = useSenseLinksQuery();
-  const [wordLinkState, execWordLinksQuery] = useWordLinksQuery();
-  const locationType = useSelector(selectLocationType);
-  const shouldQuery = locationType === Routes.WORD;
 
-  const lemma = useMemo(() => selectLemma(wordState), [wordState]);
-
-  const senseSynsetIds = useMemo(() => selectSenseSynsetIds(sensesState), [
-    sensesState,
-  ]);
-
-  const senses = useMemo(() => selectSenses(sensesState), [sensesState]);
-
-  const senseLinks = useMemo(() => selectSenseLinks(senseLinksState), [
-    senseLinksState,
-  ]);
-
-  const wordLinks = useMemo(() => selectWordLinks(wordLinkState), [
-    wordLinkState,
-  ]);
+  const [wordHierarchy, setWordHierarchy] = useState(
+    null as null | WordHierarchy
+  );
 
   useEffect(() => {
-    if (shouldQuery && wordid) {
-      execWordQuery(wordid);
-      execWordLinksQuery(wordid);
-      execSensesQuery(wordid);
+    if (wordid) {
+      getWordHierarchy(wordid).then(setWordHierarchy);
     }
-  }, [shouldQuery, wordid]);
+  }, [wordid]);
 
-  useEffect(() => {
-    if (shouldQuery) {
-      senseSynsetIds.length && execSenseLinksQuery(senseSynsetIds);
-    }
-  }, [senseSynsetIds]);
-
-  return useMemo(() => {
-    if (wordid && lemma && senses && senseLinks && wordLinks) {
-      return {
-        wordid,
-        lemma,
-        children: zipWith(
-          mergeRight,
-          senses,
-          map(objOf("children"), map(lookUpWordLinks(wordLinks), senseLinks))
-        ),
-      };
-    }
-
-    return null;
-  }, [wordid, lemma, senses, senseLinks, wordLinks]);
+  return wordHierarchy;
 }
 
-function lookUpWordLinks(wordLinks: WordLinkResult[]) {
-  return (senseLinks: SenseLinkResult[]) =>
-    map(
-      (senseLink: SenseLinkResult) => ({
-        ...senseLink,
-        children: filter(
-          propEq("synsetid", prop("synsetid", senseLink)),
-          wordLinks
-        ),
-      }),
-      senseLinks
-    );
+async function getWordHierarchy(wordid: number) {
+  const dbExec = await getDbInstance();
+  const { results } = await dbExec(
+    [
+      formatWordQuery(wordid),
+      formatWordSensesQuery(wordid),
+      formatSenseLinksQuery(wordid),
+      formatWordLinksQuery(wordid),
+    ].join(";\n")
+  );
+
+  if (areAllResultsPresent(results)) {
+    return buildWordHierarchy(...parseResults(results));
+  }
+
+  return null;
+}
+
+const areAllResultsPresent = ifElse(
+  is(Array),
+  pipe(length, equals(4)),
+  identity
+) as (
+  results: DbResult[] | undefined
+) => results is [DbResult, DbResult, DbResult, DbResult];
+
+function parseResults(
+  res: [DbResult, DbResult, DbResult, DbResult]
+): [[Word], WordSenseResult[], SenseLinkResult[], WordLinkResult[]] {
+  return map(mapResultToObjects, res) as any;
+}
+
+function buildWordHierarchy(
+  [word]: [Word],
+  wordSenses: WordSenseResult[],
+  senseLinks: SenseLinkResult[],
+  wordLinks: WordLinkResult[]
+) {
+  return {
+    ...word,
+    dx: 0,
+    dy: 0,
+    children: map(addSenseLinks(senseLinks, wordLinks), wordSenses),
+  };
+}
+
+type WordHierarchy = ReturnType<typeof buildWordHierarchy>;
+
+const filterWithSynsetId = useWith(filter, [propEq("synsetid"), identity]);
+
+function addSenseLinks(
+  senseLinks: SenseLinkResult[],
+  wordLinks: WordLinkResult[]
+) {
+  return (wordSense: WordSenseResult) => ({
+    ...wordSense,
+    dx: 0,
+    dy: 0,
+    children: map(
+      addWordLinks(wordLinks),
+      filterWithSynsetId(wordSense.synsetid, senseLinks)
+    ),
+  });
+}
+
+function addWordLinks(wordLinks: WordLinkResult[]) {
+  return (senseLink: SenseLinkResult) => ({
+    ...senseLink,
+    dx: 0,
+    dy: 0,
+    children: filterWithSynsetId(senseLink.synset2id, wordLinks),
+  });
 }

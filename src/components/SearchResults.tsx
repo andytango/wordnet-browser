0481ba1932d529177de6bae0d097de6915978405
Andrew Hall <andrew@rioja.io>
2020-07-08 06:22:36 +0100

    add loading message to search results

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,13 +1,44 @@
 import { map, head } from "ramda";
-import { default as React } from "react";
+import { default as React, useState, useEffect } from "react";
 import { SearchResultWord, SearchWord } from "../hooks/searchWord";
 import { SearchResult } from "./SearchResult";
 
 export function SearchResults({ searchWord }: { searchWord: SearchWord }) {
   const { loading, results } = searchWord;
   const searchResults = head(results) || [];
+  const [{ timeout, shouldShowLoading }, setLoadingTimeoutState] = useState({
+    timeout: -1,
+    shouldShowLoading: false,
+  });
 
-  return !loading && Array.isArray(searchResults) ? (
+  useEffect(() => {
+    clearTimeout(timeout);
+    if (loading) {
+      setLoadingTimeoutState({
+        timeout: setTimeout(
+          () =>
+            setLoadingTimeoutState({
+              timeout: -1,
+              shouldShowLoading: true,
+            }),
+          200
+        ),
+        shouldShowLoading: false,
+      });
+    }
+
+    if (shouldShowLoading) {
+      setLoadingTimeoutState({ timeout: -1, shouldShowLoading: false });
+    }
+  }, [loading]);
+
+  if (shouldShowLoading) {
+    return (
+      <div className="text-center font-bold my-6 min-w-full">Please wait...</div>
+    );
+  }
+
+  return !loading && Array.isArray(searchResults) && searchResults.length ? (
     <>
       <div className="text-center font-bold my-6 min-w-full">
         {searchResults.length} results found
